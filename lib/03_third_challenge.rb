35145703de3f8ded9a8eb9c0cabc2d5d4abde631
Arielle ross <arielle@arielleross.com>
2020-11-20 05:07:15 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -22,7 +22,8 @@ def third_challenge
         :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+       :name => "Lady Montague",
+        :age => "54"
         },
       :heroine => {
         #your key/value pairs here
